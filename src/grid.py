842166c2236770cb37be537dd1fb182977f501fa
adrifcastr <adrifcastr@gmail.com>
2022-02-25 09:33:19 +0100

    tables, yay

--- src/grid.py
+++ src/grid.py
@@ -1,13 +1,19 @@
 import os
 from colorama import Back, Style
+from tabulate import tabulate
 
 def draw_blank_grid(word: str):
     os.system('cls||clear')
     print('Pydle\nWordle, but Python\n')
     
-    for i in range(6):
-        print('\u2B1C' * len(word))
-    print('\n')
+    grid_data = []
+    column_data = []
+
+    for x in range(len(word)): column_data.append('?')
+    for y in range(6): grid_data.append(column_data)
+
+    print(tabulate(grid_data, tablefmt="fancy_grid"))
+
 
 def update_grid(word: str, guess_list: list[str], amt: int):
     os.system('cls||clear')
